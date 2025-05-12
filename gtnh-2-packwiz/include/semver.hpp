0302200c071a79125c275710760b922453c52cf5
Pixel <scott.blatt.0b10@gmail.com>
2025-05-12 18:14:13 -0500

    default constructor

--- gtnh-2-packwiz/include/semver.hpp
+++ gtnh-2-packwiz/include/semver.hpp
@@ -16,6 +16,7 @@ class semVer {
     semVer(int major, int minor, int patch, std::string prerelease = "");
     semVer(std::string semverString);
     semVer(semVerStruct structured);
+    semVer();
     const std::string string() const;
     bool sameMaj(semVer other);
     bool sameMin(semVer other);
---
+++ gtnh-2-packwiz/src/semVer/semVer.4.cpp
@@ -0,0 +1,3 @@
+#include "semver.hpp"
+
+semVer::semVer(semVerStruct structured): version({ 0, 0, 0, "", false}) {} // Shortcut
