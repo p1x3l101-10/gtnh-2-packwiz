2487244ffaa3db845f1a9977954f747160016ad7
Pixel <scott.blatt.0b10@gmail.com>
2025-07-25 18:25:09 -0500

    constructor

--- gtnh-2-packwiz/src/pack/pack.cpp
+++ gtnh-2-packwiz/src/pack/pack.cpp
@@ -1,3 +1,6 @@
 #include "gtnh2Packwiz/pack.hpp"
 
-gtnh2Packwiz::pack::pack(std::string version, std::shared_ptr<gtnh2Packwiz::configFile> config) {}
+gtnh2Packwiz::pack::pack(std::string version, std::shared_ptr<gtnh2Packwiz::configFile> configPtr)
+: packVersion(version)
+, config(configPtr)
+{}
