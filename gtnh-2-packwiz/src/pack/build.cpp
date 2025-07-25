c1cf72abc54784990c8cfd19f677e0a2135dd5d5
Pixel <scott.blatt.0b10@gmail.com>
2025-07-25 18:38:35 -0500

    plan out function

--- gtnh-2-packwiz/src/pack/build.cpp
+++ gtnh-2-packwiz/src/pack/build.cpp
@@ -1,3 +1,13 @@
 #include "gtnh2Packwiz/pack.hpp"
 
-void gtnh2Packwiz::pack::build() {}
+void gtnh2Packwiz::pack::build() {
+  // Download zip
+  // Parse version
+  // Get files from config repo
+  // Read mods to metadata container
+  // Serialize container to packwiz metafiles
+  // Do the same for everything in the config repo
+  // Write an unsup config file
+  // Hash everything and create an index.toml file
+  // Create the pack.toml file
+}
