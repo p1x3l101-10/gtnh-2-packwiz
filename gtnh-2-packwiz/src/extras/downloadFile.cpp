50d77a324379fb0f81d0425ea24414fa6e160371
Pixel <scott.blatt.0b10@gmail.com>
2025-10-06 15:08:21 -0500

    fix writer

--- gtnh-2-packwiz/src/extras/downloadFile.cpp
+++ gtnh-2-packwiz/src/extras/downloadFile.cpp
@@ -1,5 +1,4 @@
 #include <cstddef>
-#include <cstdio>
 #include <curlpp/Easy.hpp>
 #include <curlpp/Exception.hpp>
 #include <curlpp/Option.hpp>
@@ -9,50 +8,23 @@
 #include <string>
 #include "config.hpp"
 #include "gtnh2Packwiz/extras.hpp"
+#include <fstream>
 
 namespace fs = std::filesystem;
 using fs::path;
 using std::string;
 
-// Create a file object so that I avoid bugs
-class fileOb {
-  private:
-    FILE* file = nullptr;
-  public:
-    fileOb(const std::string filename, const std::string writeMode) {
-      file = fopen(filename.c_str(), writeMode.c_str()); // SEGFAULTS HERE
-    }
-    ~fileOb() {
-      close();
-    }
-    void close() {
-      fclose(file);
-    }
-    FILE* getFile() {
-      return file;
-    }
-};
-
-// Writing callback
-static size_t writeFileCallback(char* ptr, size_t size, size_t nmemb, void* f) {
-  FILE* file = (FILE*)f;
-  return fwrite(ptr, size, nmemb, file);
-}
 void gtnh2Packwiz::extras::downloadFile(string url, path destination) {
   log4cpp::Category& logger = log4cpp::Category::getInstance(NAME ".extras.downloadFile");
   try {
     logger.infoStream() << "Downloading URL: '" << url << "'";
     curlpp::Easy request;
-    // Set the callback to a file writer
-    curlpp::options::WriteFunctionCurlFunction writer(writeFileCallback);
-    fileOb file(destination, "wb");
-    curlpp::OptionTrait<void*, CURLOPT_WRITEDATA> data(file.getFile());
+    // Write to file
+    std::ofstream outFile(destination);
     // Create the request
-    request.setOpt(writer);
-    request.setOpt(data);
     request.setOpt(new curlpp::options::Url(url));
+    request.setOpt(new curlpp::options::WriteStream(&outFile));
     request.perform();
-    file.close();
     return; // Done now
   } catch (curlpp::LogicError& e) {
     logger.fatalStream() << e.what();
