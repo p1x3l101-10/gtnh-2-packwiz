bd3ec4733a8f413d754d1cb6f2a7100acfd0f629
Pixel <scott.blatt.0b10@gmail.com>
2025-10-06 09:55:55 -0500

    formatting

--- gtnh-2-packwiz/include/gtnh2Packwiz/init.hpp
+++ gtnh-2-packwiz/include/gtnh2Packwiz/init.hpp
@@ -4,11 +4,11 @@
 #include <utility>
 
 namespace gtnh2Packwiz::init {
-#ifdef DEFINE_ARGS
-  boost::program_options::variables_map args;
-#else
-  extern boost::program_options::variables_map args;
-#endif
+# ifdef DEFINE_ARGS
+    boost::program_options::variables_map args;
+# else
+    extern boost::program_options::variables_map args;
+# endif
   log4cpp::Priority::PriorityLevel getPriority();
   void argProcesser(std::pair<int, char**> args);
   void logger();
