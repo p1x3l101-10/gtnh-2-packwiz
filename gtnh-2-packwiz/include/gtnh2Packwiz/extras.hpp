8bf72c8616606c479e14311b91e098ca4c8e6452
Pixel <scott.blatt.0b10@gmail.com>
2025-10-08 13:41:58 -0500

    start the destination

--- gtnh-2-packwiz/include/gtnh2Packwiz/extras.hpp
+++ gtnh-2-packwiz/include/gtnh2Packwiz/extras.hpp
@@ -2,11 +2,25 @@
 
 #include <cstdint>
 #include <filesystem>
+#include <vector>
 #include <string>
+#include <algorithm>
+#include <iterator>
 #include "config.hpp"
 
 namespace gtnh2Packwiz::extras {
     void downloadFile(std::string url, std::filesystem::path destination);
     void extractZip(std::filesystem::path zip, std::filesystem::path outDir);
     const std::string humanReadableBytes(uint64_t size, unsigned precision = 0);
+    // Why must templates be horrible like this???
+    template <typename T>
+        std::vector<T> setIntersection(std::vector<T> a, std::vector<T> b) {
+            std::vector<T> result;
+            // Sort lists
+            std::sort(a.begin(), a.end());
+            std::sort(b.begin(), b.end());
+            // Call the actual STL function i am wrapping
+            std::set_intersection(a.begin(), a.end(), b.begin(), b.end(), std::inserter(result, result.begin()));
+            return result;
+        };
 } // namespace gtnh2Packwiz::extras
--- gtnh-2-packwiz/src/pack/build.cpp
+++ gtnh-2-packwiz/src/pack/build.cpp
@@ -1,31 +1,48 @@
+#include <algorithm>
 #include <boost/asio/post.hpp>
 #include <filesystem>
+#include <fstream>
 #include <memory>
 #include <string>
+#include <vector>
 #include "config.hpp"
 #include "gtnh2Packwiz/extras.hpp"
 #include "gtnh2Packwiz/pack.hpp"
 #include "gtnh2Packwiz/poolManager.hpp"
 #include "loggerMacro.hpp"
-
-extern boost::asio::thread_pool tp;
+#include <nlohmann/json.hpp>
 
 extern gtnh2Packwiz::poolManager pool;
+
+extern void shutdown(bool fatal = false, bool silent = false);
 
 namespace fs = std::filesystem;
 using boost::asio::post;
 using fs::path;
 using std::string;
+using std::vector;
+using nlohmann::json;
+using std::ifstream;
+
+template vector<string> gtnh2Packwiz::extras::setIntersection<string>(std::vector<string> a, std::vector<string> b);
 
 void gtnh2Packwiz::pack::build() {
     createLogger(logger);
 
     path packDir = CACHE "/pack";
     path configDir = CACHE "/config";
+    path destDir = CACHE "/packwiz";
 
     // Create cache directory
-    logger.debugStream() << "Created cache at: " << CACHE;
-    fs::create_directories(CACHE);
+    {
+        if (fs::exists(CACHE)) {
+            logger.debugStream() << "Rereated cache at: " << CACHE;
+            fs::remove_all(CACHE);
+        } else {
+            logger.debugStream() << "Created cache at: " << CACHE;
+        }
+        fs::create_directories(CACHE);
+    }
 
     {
         path packZip = packDir.string() + ".zip";
@@ -53,6 +70,51 @@ void gtnh2Packwiz::pack::build() {
             logger.info("Files extracted");
         }
     }
+    // Parse the modpack json for data
+    json gtnhModpack;
+    {
+        path gtnhModpackJSONFile = packDir.string() + "/DreamAssemblerXXL-master/gtnh-modpack.json";
+        ifstream gtnhModpackJSON(gtnhModpackJSONFile);
+        gtnhModpackJSON >> gtnhModpack;
+    }
+
+    // Create the basic file structure
+    {
+        // Config has the raw files already
+        {
+            logger.debug("Coping config repo to destination");
+            // Copy files
+            path realCFG = configDir.string() + "/GT-New-Horizons-Modpack-" + packVersion.string();
+            fs::copy(realCFG, destDir);
+        }
+        // Find files that are excluded in both client and server so we can delete them
+        {
+            logger.info("Deleting files excluded on server and client");
+            // Sanity checks
+            if (! gtnhModpack["server_exclusions"].is_array()) {
+                logger.fatal("Expected 'server_exclusions' to be an array!");
+                shutdown(true);
+            }
+            if (! gtnhModpack["client_exclusions"].is_array()) {
+                logger.fatal("Expected 'client_exclusions' to be an array!");
+                shutdown(true);
+            }
+            // Convert into useful types
+            vector<string> serverExclude = gtnhModpack["server_exclusions"].get<vector<string>>();
+            vector<string> clientExclude = gtnhModpack["client_exclusions"].get<vector<string>>();
+            // Get the intersection
+            vector<string> allExclude = gtnh2Packwiz::extras::setIntersection<string>(serverExclude, clientExclude);
+            // Delete files that match the intersection
+            for (const auto &badFile : allExclude) {
+                path realPath = destDir.string() + "/" + badFile;
+                logger.debugStream() << "Deleting file: '" << realPath.string() << "'";
+                fs::remove(realPath);
+            }
+        }
+    }
+    // Construct an index toml
+    
+    // OLD TODOS
     // Parse version
     // Get files from config repo
     // Read mods to metadata container
