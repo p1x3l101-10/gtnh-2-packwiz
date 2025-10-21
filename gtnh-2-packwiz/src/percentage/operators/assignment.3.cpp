d884bd21a22ee119e5615fc63ed8ac29c1981239
Pixel <scott.blatt.0b10@gmail.com>
2025-10-21 12:17:50 -0500

    fix progress

--- gtnh-2-packwiz/src/percentage/operators/assignment.3.cpp
+++ gtnh-2-packwiz/src/percentage/operators/assignment.3.cpp
@@ -1,8 +1,6 @@
 #include "percentage.hpp"
 
 percentage& percentage::operator=(const percentage& other) {
-    if (this != &other) {
-        value = other.value;
-    }
+    this->value = other.value;
     return *this;
 }
