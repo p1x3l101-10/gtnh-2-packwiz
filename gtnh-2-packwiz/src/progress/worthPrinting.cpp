670604d501e3b8d4e1c740ecb9b5000c493e4eed
Pixel <scott.blatt.0b10@gmail.com>
2025-10-22 08:59:44 -0500

    deal with config logic

--- gtnh-2-packwiz/src/progress/worthPrinting.cpp
+++ gtnh-2-packwiz/src/progress/worthPrinting.cpp
@@ -1,10 +1,17 @@
 #include "progress.hpp"
+#include "config.hpp"
 
 bool progress::worthPrinting() {
+    if constexpr (!PRINT_PERCENTAGES) {
+        // Completely disable print logic
+        return false;
+    }
     percentage mustSurpass = lastPrinted + minimumChange;
     if (mustSurpass < value) {
         lastPrinted = value;
-        return true;
+        if constexpr (INTERCEPT_PRINTING_PROGRESS_FOR_BAR) {} else {
+            return true;
+        }
     }
     return false;
 }
